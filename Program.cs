960e9e70ebe44da92ba5ed0f29c90e3ddb71b5f7
Marius Alwan Meyer <22202402+Sporqist@users.noreply.github.com>
2022-05-23 11:38:42 +0200

    - Formatierung - Vollständige Ausgabe

--- Program.cs
+++ Program.cs
@@ -17,22 +17,22 @@ namespace Preiskalkulation
             Console.WriteLine("=== Listenpreiskalkulation ===");
             Console.WriteLine();
             Console.WriteLine($"Die Eingabe der Listeneinkaufspreise erfolgt in {region.CurrencySymbol}.");
-            Console.WriteLine("Eingaben mit \"Enter\" bestätigan. Zum Beenden und ausgeben der Ergebnisse \"exit\" eingeben.");
+            Console.WriteLine("Eingaben mit \"Enter\" bestätigen. Zum Beenden und ausgeben der Ergebnisse \"exit\" eingeben.");
             Console.WriteLine();
 
             bool Exit;
             int Iteration = 1;
-            Queue<double> Ergebnisse = new Queue<double>();
+            Queue<double[]> Ergebnisse = new Queue<double[]>();
             
             Console.WriteLine("Eingabe:");
             Console.WriteLine($"Nr.\t| Listeneinkaufspreis ({region.CurrencySymbol})");
-            Console.WriteLine("--------+------------------------");
+            Console.WriteLine("========+==========================================");
             do {
                 (double, bool) _Eingabe = Eingabe($"{Iteration}\t| ");
                 double Listeneinkaufspreis = _Eingabe.Item1;
                 Exit = _Eingabe.Item2;
 
-                double Ergebnis = Kalkulation(Listeneinkaufspreis);
+                double[] Ergebnis = Kalkulation(Listeneinkaufspreis);
                 Ergebnisse.Enqueue(Ergebnis);
                 Iteration++;
             } while (!Exit);
@@ -40,16 +40,32 @@ namespace Preiskalkulation
             Console.WriteLine();
             Console.WriteLine("Ergebnisse:");
             Console.WriteLine($"Nr.\t| Listenverkaufspreis ({region.CurrencySymbol})");
-            Console.WriteLine("--------+------------------------");
+            Console.WriteLine("========+==========================================");
 
             for (int i = 1; i <= Ergebnisse.Count; i++) {
-                double Ausgabe = Ergebnisse.Dequeue();
+                double[] Ausgabe = Ergebnisse.Dequeue();
 
-                Console.WriteLine($"{i}\t| {Ausgabe}");
+                Console.WriteLine($"{i}\t| Listeneinkaufspreis\t= {Ausgabe[0]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Lieferrabatt\t\t↑ -{Ausgabe[1]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Zieleinkaufspreis\t= {Ausgabe[2]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Liefererskonto\t↑ - {Ausgabe[3]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Bareinkaufspreis\t= {Ausgabe[4]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Bezugskosten\t\t↑ +{Ausgabe[5]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Bezugspreis\t\t= {Ausgabe[6]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Handlungskosten\t↑ + {Ausgabe[7]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Selbstkosten\t\t= {Ausgabe[8]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Gewinn\t\t↑ + {Ausgabe[9]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Barverkaufspreis\t= {Ausgabe[10]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Kundenskonto\t\t↓ -{Ausgabe[11]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Vertreterprovision\t↓ - {Ausgabe[12]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Zielverkaufspreis\t= {Ausgabe[13]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Kundenrabatt\t\t↓ - {Ausgabe[14]} {region.CurrencySymbol}");
+                Console.WriteLine($"\t| Listenverkaufspreis\t= {Ausgabe[15]} {region.CurrencySymbol}");
+                Console.WriteLine("--------+------------------------------------------");
             }
         }
 
-        static double Kalkulation(double _Listeneinkaufspreis) {
+        static double[] Kalkulation(double _Listeneinkaufspreis) {
             
             double _Lieferrabatt        = 0.20;
             double _Liefererskonto      = 0.03;
@@ -59,28 +75,31 @@ namespace Preiskalkulation
             double _Kundenskonto        = 0.02;
             double _Vertreterprovision  = 0.05;
             double _Kundenrabatt        = 0.10;
-
-
-            double Listeneinkaufspreis           = Math.Round(_Listeneinkaufspreis, 2);
-            double Zieleinkaufspreis             = Math.Round(Listeneinkaufspreis - Listeneinkaufspreis * _Lieferrabatt, 2);
-            double Bareinkaufspreis              = Math.Round(Zieleinkaufspreis - Zieleinkaufspreis * _Liefererskonto, 2);
-            double Bezugspreis                   = Math.Round(Bareinkaufspreis + _Bezugskosten, 2);
-            double Selbstkosten                  = Math.Round(Bezugspreis + Bezugspreis * _Handlungskosten, 2);
-            double Barverkaufspreis              = Math.Round(Selbstkosten + Selbstkosten * _Gewinn, 2);
-            
             _Kundenskonto       *= 100;
             _Vertreterprovision *= 100;
             _Kundenrabatt       *= 100;
-            double Zielverkaufspreis_Modifikator = 100 - (_Kundenskonto + _Vertreterprovision);
-            double Zielverkaufspreis_Basis       = Barverkaufspreis / Zielverkaufspreis_Modifikator;
-            double Zielverkaufspreis             = Math.Round(Barverkaufspreis
-                                                   + Math.Round(Zielverkaufspreis_Basis * _Kundenskonto, 2) 
-                                                   + Math.Round(Zielverkaufspreis_Basis * _Vertreterprovision, 2), 2);
+
+            double Listeneinkaufspreis             = Math.Round(_Listeneinkaufspreis, 2);
+            double Lieferrabatt                    = Math.Round(Listeneinkaufspreis * _Lieferrabatt, 2);
+            double Zieleinkaufspreis               = Math.Round(Listeneinkaufspreis - Lieferrabatt, 2);
+            double Liefererskonto                  = Math.Round(Zieleinkaufspreis * _Liefererskonto, 2);
+            double Bareinkaufspreis                = Math.Round(Zieleinkaufspreis - Liefererskonto, 2);
+            double Bezugspreis                     = Math.Round(Bareinkaufspreis + _Bezugskosten, 2);
+            double Handlungskosten                 = Math.Round(Bezugspreis * _Handlungskosten, 2);
+            double Selbstkosten                    = Math.Round(Bezugspreis + Handlungskosten, 2);
+            double Gewinn                          = Math.Round(Selbstkosten * _Gewinn, 2);
+            double Barverkaufspreis                = Math.Round(Selbstkosten + Gewinn, 2);
+            
+            double Zielverkaufspreis_Modifikator   = 100 - (_Kundenskonto + _Vertreterprovision);
+            double Zielverkaufspreis_Basis         = Barverkaufspreis / Zielverkaufspreis_Modifikator;
+            double Kundenskonto                    = Math.Round(Zielverkaufspreis_Basis * _Kundenskonto, 2);
+            double Vertreterprovision              = Math.Round(Zielverkaufspreis_Basis * _Vertreterprovision, 2);
+            double Zielverkaufspreis               = Math.Round(Barverkaufspreis + Kundenskonto + Vertreterprovision, 2);
 
             double Listenverkaufspreis_Modifikator = 100 - (_Kundenrabatt);
             double Listenverkaufspreis_Basis       = Zielverkaufspreis / Listenverkaufspreis_Modifikator;
-            double Listenverkaufspreis           = Math.Round(Zielverkaufspreis
-                                                   + Math.Round(Listenverkaufspreis_Basis * _Kundenrabatt, 2), 2);
+            double Kundenrabatt                    = Math.Round(Listenverkaufspreis_Basis * _Kundenrabatt, 2);
+            double Listenverkaufspreis             = Math.Round(Zielverkaufspreis + Kundenrabatt, 2);
             
             //Console.WriteLine($"Listeneinkaufspreis: {Listeneinkaufspreis}");
             //Console.WriteLine($"Zieleinkaufspreis:   {Zieleinkaufspreis}");
@@ -93,7 +112,10 @@ namespace Preiskalkulation
             //Console.WriteLine();
 
 
-            return Listenverkaufspreis;
+            return new double[] {Listeneinkaufspreis, Lieferrabatt, Zieleinkaufspreis, Liefererskonto,
+                                 Bareinkaufspreis, _Bezugskosten, Bezugspreis, Handlungskosten, Selbstkosten, 
+                                 Gewinn, Barverkaufspreis, Kundenskonto, Vertreterprovision, Zielverkaufspreis, 
+                                 Kundenrabatt, Listenverkaufspreis};
         }
 
         static (double, bool) Eingabe(string frage) {

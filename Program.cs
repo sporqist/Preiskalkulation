466a5043cca3c241ba52c42e30f6cb7514ac4efa
Marius Alwan Meyer <22202402+Sporqist@users.noreply.github.com>
2022-05-23 10:47:11 +0200

    - Ein- und Ausgabe in Tabellenform - Hardcode Variablen für die Kalkulation - Punkt und Komma können für nachkommastellen verwendet werden

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 
@@ -10,24 +11,55 @@ namespace Preiskalkulation
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-
-            Console.WriteLine();
-            Console.WriteLine("Listenpreiskalkulation");
-            Console.WriteLine();
-
             var region = new RegionInfo(System.Threading.Thread.CurrentThread.CurrentCulture.LCID);
+            
+            Console.WriteLine();
+            Console.WriteLine("=== Listenpreiskalkulation ===");
+            Console.WriteLine();
+            Console.WriteLine($"Die Eingabe der Listeneinkaufspreise erfolgt in {region.CurrencySymbol}.");
+            Console.WriteLine("Eingaben mit \"Enter\" bestätigan. Zum Beenden und ausgeben der Ergebnisse \"exit\" eingeben.");
+            Console.WriteLine();
 
-            double _Listeneinkaufspreis = EingabeDouble($"Einkauf Listenpreis ({region.CurrencySymbol})");
-            double _Lieferrabatt        = EingabeDouble("Lieferrabatt (%)") / 100;
-            double _Liefererskonto      = EingabeDouble("Liefererskonto (%)") / 100;
-            double _Bezugskosten        = EingabeDouble($"Bezugskosten ({region.CurrencySymbol})");
-            double _Handlungskosten     = EingabeDouble("Handlungskosten (%)") / 100;
-            double _Gewinn              = EingabeDouble("Gewinn (%)") / 100;
-            double _Kundenskonto        = EingabeDouble("Kundenskonto (%)");
-            double _Vertreterprovision  = EingabeDouble("Vertreterprovision (%)");
-            double _Kundenrabatt        = EingabeDouble("Kundenrabatt (%)");
+            bool Exit;
+            int Iteration = 1;
+            Queue<double> Ergebnisse = new Queue<double>();
+            
+            Console.WriteLine("Eingabe:");
+            Console.WriteLine($"Nr.\t| Listeneinkaufspreis ({region.CurrencySymbol})");
+            Console.WriteLine("--------+------------------------");
+            do {
+                (double, bool) _Eingabe = Eingabe($"{Iteration}\t| ");
+                double Listeneinkaufspreis = _Eingabe.Item1;
+                Exit = _Eingabe.Item2;
+
+                double Ergebnis = Kalkulation(Listeneinkaufspreis);
+                Ergebnisse.Enqueue(Ergebnis);
+                Iteration++;
+            } while (!Exit);
 
             Console.WriteLine();
+            Console.WriteLine("Ergebnisse:");
+            Console.WriteLine($"Nr.\t| Listenverkaufspreis ({region.CurrencySymbol})");
+            Console.WriteLine("--------+------------------------");
+
+            for (int i = 1; i <= Ergebnisse.Count; i++) {
+                double Ausgabe = Ergebnisse.Dequeue();
+
+                Console.WriteLine($"{i}\t| {Ausgabe}");
+            }
+        }
+
+        static double Kalkulation(double _Listeneinkaufspreis) {
+            
+            double _Lieferrabatt        = 0.20;
+            double _Liefererskonto      = 0.03;
+            double _Bezugskosten        = 5.40;
+            double _Handlungskosten     = 0.3215;
+            double _Gewinn              = 0.15;
+            double _Kundenskonto        = 0.02;
+            double _Vertreterprovision  = 0.05;
+            double _Kundenrabatt        = 0.10;
+
 
             double Listeneinkaufspreis           = Math.Round(_Listeneinkaufspreis, 2);
             double Zieleinkaufspreis             = Math.Round(Listeneinkaufspreis - Listeneinkaufspreis * _Lieferrabatt, 2);
@@ -36,6 +68,9 @@ namespace Preiskalkulation
             double Selbstkosten                  = Math.Round(Bezugspreis + Bezugspreis * _Handlungskosten, 2);
             double Barverkaufspreis              = Math.Round(Selbstkosten + Selbstkosten * _Gewinn, 2);
             
+            _Kundenskonto       *= 100;
+            _Vertreterprovision *= 100;
+            _Kundenrabatt       *= 100;
             double Zielverkaufspreis_Modifikator = 100 - (_Kundenskonto + _Vertreterprovision);
             double Zielverkaufspreis_Basis       = Barverkaufspreis / Zielverkaufspreis_Modifikator;
             double Zielverkaufspreis             = Math.Round(Barverkaufspreis
@@ -47,31 +82,39 @@ namespace Preiskalkulation
             double Listenverkaufspreis           = Math.Round(Zielverkaufspreis
                                                    + Math.Round(Listenverkaufspreis_Basis * _Kundenrabatt, 2), 2);
             
-            Console.WriteLine($"Listeneinkaufspreis: {Listeneinkaufspreis}");
-            Console.WriteLine($"Zieleinkaufspreis:   {Zieleinkaufspreis}");
-            Console.WriteLine($"Bareinkaufspreis:    {Bareinkaufspreis}");
-            Console.WriteLine($"Bezugspreis:         {Bezugspreis}");
-            Console.WriteLine($"Selbstkosten:        {Selbstkosten}");
-            Console.WriteLine($"Barverkaufspreis:    {Barverkaufspreis}");
-            Console.WriteLine($"Zielverkaufspreis:   {Zielverkaufspreis}");
-            
-            Console.WriteLine();
-            
-            Console.WriteLine($"Der Listenverkaufspreis beträgt: {Listenverkaufspreis}€ netto");
+            //Console.WriteLine($"Listeneinkaufspreis: {Listeneinkaufspreis}");
+            //Console.WriteLine($"Zieleinkaufspreis:   {Zieleinkaufspreis}");
+            //Console.WriteLine($"Bareinkaufspreis:    {Bareinkaufspreis}");
+            //Console.WriteLine($"Bezugspreis:         {Bezugspreis}");
+            //Console.WriteLine($"Selbstkosten:        {Selbstkosten}");
+            //Console.WriteLine($"Barverkaufspreis:    {Barverkaufspreis}");
+            //Console.WriteLine($"Zielverkaufspreis:   {Zielverkaufspreis}");
+            //
+            //Console.WriteLine();
+
+
+            return Listenverkaufspreis;
         }
 
-        static double EingabeDouble(string frage) {
-            double eingabe = 0;
+        static (double, bool) Eingabe(string frage) {
+            double eingabe_ = 0;
+            bool exit_ = false;
 
             bool input_success = false;
             while (!input_success) {
-                Console.Write(frage + ": ");
+                Console.Write(frage);
+
                 string console_input = Console.ReadLine();
-                if (double.TryParse(console_input, out eingabe) == false) {
+                if (console_input == "exit") {
+                    exit_ = true;
+                }
+
+                console_input = console_input.Replace(',', '.');
+                if (double.TryParse(console_input, NumberStyles.Any, CultureInfo.InvariantCulture, out eingabe_) == false && exit_ == false) {
                     Console.WriteLine("Ihre Eingabe war fehlerhaft. Bitte probieren sie es nocheinmal.");
                 } else { input_success = true; }
             }
-            return eingabe;
+            return (eingabe_, exit_);
         }
     }
 }
